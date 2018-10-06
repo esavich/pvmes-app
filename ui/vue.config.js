3beb5624359cbb01e79fb9e0201d988bed3f63e6
Evgeniy Savich <evgeniy@directline.pro>
2018-10-06 15:11:05 +0700

    инициализировал vue.js приложение

---
+++ ui/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    devServer: {
+        host: '0.0.0.0',
+        port: 8081,
+        proxy: {
+            '/api': {
+                target: 'http://nginx:80/api',
+                changeOrigin: true
+            }
+        }
+    }
+}
