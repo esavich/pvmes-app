83f332209392a5c350d1f71ab17f731ac4090207
Evgeniy Savich <evgeniy@directline.pro>
2018-10-15 22:40:28 +0700

    Убрал лишние роуты на фронтенде

--- ui/src/router.js
+++ ui/src/router.js
@@ -7,41 +7,33 @@ Vue.use(Router);
 
 export default new Router({
     // mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-        path: '/page/:page',
-        name: 'homePaged',
-        component: Home
-    },
-      {
-          path: '/tag/:tag',
-          name: 'tagged',
-          component: Home,
-          children: [
-              {
-                  path: 'page/:page',
-                  name: 'taggedPaged',
-                  component: Home
-              }
-          ]
-      },
-      {
-          path: '/post/:id',
-          name: 'post',
-          component: Post
-      },
-      // {
-      //   path: '/about',
-      //   name: 'about',
-      //   // route level code-splitting
-      //   // this generates a separate chunk (about.[hash].js) for this route
-      //   // which is lazy-loaded when the route is visited.
-      //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-      // }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/page/:page',
+            name: 'homePaged',
+            component: Home
+        },
+        {
+            path: '/tag/:tag',
+            name: 'tagged',
+            component: Home,
+            children: [
+                {
+                    path: 'page/:page',
+                    name: 'taggedPaged',
+                    component: Home
+                }
+            ]
+        },
+        {
+            path: '/post/:id',
+            name: 'post',
+            component: Post
+        }
+    ]
 })
